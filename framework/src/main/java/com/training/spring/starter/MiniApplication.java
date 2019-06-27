0267740afeee3d68e13099356df65c13a4a5854a
weibin.wang01 <weibin.wang01@ucarinc.com>
2019-06-27 10:27:22 +0800

    complete basic feature of servlet, relate tomcat and servlet;

--- framework/src/main/java/com/training/spring/starter/MiniApplication.java
+++ framework/src/main/java/com/training/spring/starter/MiniApplication.java
@@ -1,5 +1,8 @@
 package com.training.spring.starter;
 
+import com.training.spring.web.server.TomcatServer;
+import org.apache.catalina.LifecycleException;
+
 /**
  * @author Wagic
  * @date : 2019/6/25 20:44
@@ -8,5 +11,11 @@ public class MiniApplication {
 
     public static void run(Class<?> cls, String[] args) {
         System.out.println("Hello mini-spring!");
+        TomcatServer tomcatServer = new TomcatServer(args);
+        try {
+            tomcatServer.startServer();
+        } catch (LifecycleException e) {
+            e.printStackTrace();
+        }
     }
 }
--- framework/src/main/java/com/training/spring/web/server/TomcatServer.java
+++ framework/src/main/java/com/training/spring/web/server/TomcatServer.java
@@ -1,6 +1,9 @@
 package com.training.spring.web.server;
 
+import com.training.spring.web.servlet.TestServlet;
+import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
+import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
 
 /**
@@ -12,7 +15,7 @@ public class TomcatServer {
     private Tomcat tomcat;
     private String[] args;
 
-    TomcatServer(String[] args) {
+    public TomcatServer(String[] args) {
         this.args = args;
     }
 
@@ -20,6 +23,27 @@ public class TomcatServer {
         tomcat = new Tomcat();
         tomcat.setPort(6699);
         tomcat.start();
+
+        /*
+         * tomcat容器有四层：Engine，Context，Wrapper，Servlet
+         * 创建标准Context容器
+         */
+        Context context = new StandardContext();
+        context.setPath("");
+        // 添加生命周期监听
+        context.addLifecycleListener(new Tomcat.FixContextListener());
+
+        // 实例化Servlet对象，servlet注册到到Tomcat容器中
+        TestServlet servlet = new TestServlet();
+        // 用tomcat的静态方法，将Servlet对象注册到context容器内
+        Tomcat.addServlet(context, "testServlet", servlet)
+                // 支持异步
+                .setAsyncSupported(true);
+
+        // 添加一个Servlet到URI的映射，访问URI时，将会调用这个servlet
+        context.addServletMappingDecoded("/test.json", "testServlet");
+        // Context容器需要依附在host容器，所以在这里将context对象注册到默认的host容器
+        tomcat.getHost().addChild(context);
 
         Thread awaitThread = new Thread("tomcat_await_thread") {
             @Override
