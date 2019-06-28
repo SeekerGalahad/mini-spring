dcc8155947b5cc7ac6baa5343b05870537f25705
weibin.wang01 <weibin.wang01@ucarinc.com>
2019-06-28 18:30:26 +0800

    add annotation process

--- framework/src/main/java/com/training/spring/web/handler/HandlerManager.java
+++ framework/src/main/java/com/training/spring/web/handler/HandlerManager.java
@@ -1,5 +1,11 @@
 package com.training.spring.web.handler;
 
+import com.training.spring.web.mvc.Controller;
+import com.training.spring.web.mvc.RequestMapping;
+import com.training.spring.web.mvc.RequestParam;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,9 +15,32 @@ import java.util.List;
  */
 public class HandlerManager {
 
-    private static List<MappingHandler> mappingHandlerList = new ArrayList<>();
+    private static List<MappingHandler> mappingHandlerList =
+            new ArrayList<>();
 
+    public static void resolveMappingHandler(List<Class<?>> classList) {
+        for (Class<?> cls : classList) {
+            if (cls.isAnnotationPresent(Controller.class)) {
+                parseHandlerFromController(cls);
+            }
+        }
+    }
 
+    private static void parseHandlerFromController(Class<?> cls) {
+        Method[] methods = cls.getDeclaredMethods();
+        for (Method method : methods) {
+            if (!method.isAnnotationPresent(RequestMapping.class)) {
+                continue;
+            }
+            String uri = method.getDeclaredAnnotation(RequestMapping.class).value();
+            List<String> paramNameList = new ArrayList<>();
+            for (Parameter parameter : method.getParameters()) {
+                if (parameter.isAnnotationPresent(RequestParam.class)) {
+
+                }
+            }
+         }
+    }
 
 
 }
